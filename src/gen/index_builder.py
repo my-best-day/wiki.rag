3bfbcd5d755fbd5977f10088848ce75c64072b67
Ron Hirsch <ronhirsch@gmail.com>
2024-10-24 09:58:46 -0400

    split to builder / validator / dumper

--- src/gen/index_builder.py
+++ src/gen/index_builder.py
@@ -1,3 +1,14 @@
+"""
+Build an index of articles and their paragraphs which includes the article/paragraph
+offset in the file (in bytes, suitable for file_handle.seek()), and byte, text, and clean-text
+lengths. Byte length is used to read the text from the file and clean-text length to calculate
+segments for encoding / embedding
+
+Here we have
+- IndexBuilder: builds the index (articles and paragraphs)
+- IndexValidator: validates the offsets and byte lengths
+- IndexDumper: dumps the index in a human readable format
+"""
 import re
 import time
 import logging
@@ -6,58 +17,11 @@ from pathlib import Path
 from typing import Generator, List
 from gen.element.article import Article
 from gen.element.section import Section
-from gen.element.segment import Segment
 from gen.element.header import Header
 from gen.element.paragraph import Paragraph
 from gen.element.chunk import Chunk
 
 logger = logging.getLogger(__name__)
-
-
-class Segment(Section):
-    def __init__(self, offset: int):
-        super().__init__(offset, bytes)
-        self.sections = []
-        self._byte_len = 0
-
-    @property
-    def byte_length(self) -> int:
-        return self._byte_len
-
-    def append_sections(self, sections: List[Section], max_len: int, max_overlap: int) -> None:
-        article_segments = []
-        segment = Segment()
-        article_segments.append(segment)
-
-        while sections:
-            section = sections.shift()
-
-            if self.can_add_section_to_current_segment(segment, section, max_len):
-                # simple case, just add section
-                segment.append_section(section)
-                continue
-
-            if self.is_segment_empty(segment):
-                # if first section is too long, split it
-                first, remainder = section.split(max_len)
-                segment.append(first)
-                sections.unshift(remainder)
-                continue
-
-            # section is too long, add overlap to the end of the current segment
-            overlap = min(max_overlap, max_len - segment.clean_length)
-            segment.append_section(section[-overlap:])
-
-            # create a new segment
-
-            self.sections.append(section)
-            self._byte_len += section.byte_len
-
-    def can_add_section_to_current_segment(self, segment: Segment, section: Section, max_len: int):
-        return segment.clean_length + section.clean_length <= max_len
-
-    def is_segment_empty(self, segment: Segment):
-        return segment.clean_length == 0
 
 
 class ByteReader:
@@ -70,43 +34,17 @@ class ByteReader:
         return self.file.read(size)
 
 
-# class Segmentizer:
-
-#     def __init__(self, max_len: int, min_overlap: int, max_overlap: int):
-#         self.max_len = max_len
-#         self.min_overlap = min_overlap
-#         self.max_overlap = max_overlap
-
-#     def segmentize_article(self, article: Article):
-#         self.segmentize_sections(article.sections)
-
-#     def segmentize_sections(self, sections: List[Section]):
-#         segments = []
-#         segment_length = 0
-#         for section in sections:
-#             if segment_length + section.char_length <= self.max_len:
-#                 segment_length += section.char_length
-#                 segments.append(section)
-#             else:
-#                 break
-#         return segments
-
-#     def segmentize_article(self, article: Article):
-#         segments = []
-#     segment_length = 0
-#     for section in article.sections:
-
-#         if segment_length + section.char_len <= max_len:
-#             segment_length += section.char_len
-#             segments.append(section)
-#         elif segment_length == 0:
-
-#             break
+def format_text(bytes: bytes) -> str:
+    text = bytes.decode('utf-8')
+    if len(text) > 200:
+        m = 100
+        return text[:m] + "...." + text[-m:]
+    return text
 
 
 class IndexBuilder:
 
-    CHUNK_SIZE = 2 ** 15  # 32KB
+    CHUNK_SIZE_BYTES = 2 ** 15  # 32KB
 
     # looks for lines that looks like ' = Heading 1 = '
     ARTICLE_HEADER_PATTERN = br'^\s*=\s+[^=].*[^=]\s+=\s*\n'
@@ -150,82 +88,83 @@ class IndexBuilder:
                         text = b""
                 offset += length
 
-    def dump(self):
-        print("Number of articles:", len(self.articles))
-        for article in self.articles:
-            print("." * 80)
-            for section in article.elements:
-                self.say(section)
-            print("<" * 80)
-
-    # def build_segments(self):
-    #     hard_max = self.args.max_len
-    #     for article in self.articles:
-    #         n = len(article.sections)
-    #         for i, section in enumerate(article.sections):
-    #             soft_factor = 0.9 if i == 0 or i == n - 1 else 0.8
-    #             soft_max = hard_max * soft_factor
-
-    #             if segment.byte_len + section.byte_len <= soft_max:
-    #                 segment.append_section(section)
-    #                 continue
-
-    #             yield segment
-
-    def say(self, section: Section):
-        see = self.reader.read(section.offset, len(section.bytes))
-        caption = section.__class__.__name__
-        caption_length = len(caption)
-
-        ok = see == section.bytes
-        print(caption, ">>>", self.format_text(section.bytes), "<<<",
-              "OK" if ok else "NOT OK")
-
-        if not ok:
-            see_caption = "SEE:"
-            see_caption_length = len(see_caption)
-            if see_caption_length < caption_length:
-                see_caption = see_caption.ljust(caption_length)
-            see_caption = see_caption[:caption_length]
-            print(see_caption, ">>>", self.format_text(see), "<<<")
-            raise ValueError("SEE does not match")
-
-    @staticmethod
-    def format_text(bytes: bytes) -> str:
-        text = bytes.decode('utf-8')
-        if len(text) > 200:
-            m = 100
-            return text[:m] + "...." + text[-m:]
-        return text
-
     def read_chunks(self) -> Generator[Section, None, None]:
         with open(self.args.text, "rb") as inp:
+            # buffer holds the leading bytes of a multi-byte Unicode character that
+            # was split between chunks and couldn't be decoded in the previous chunk
             buffer = b""
             while True:
                 offset = inp.tell()
-                chunk = inp.read(self.CHUNK_SIZE)
+                chunk = inp.read(self.CHUNK_SIZE_BYTES)
 
                 if not chunk:
                     break
 
+                # prepend buffer (leading bytes of a multi-byte Unicode character from
+                # previous chunk) to current chunk and adjust offset accordingly
                 chunk = buffer + chunk
+                offset -= len(buffer)
 
+                # check if there is a split Unicode character in the end of chunk
                 try:
                     chunk.decode('utf-8')
                     buffer = b""
                     text = chunk
+                    # print(f"Unicode ok at {offset + len(chunk)}: {chunk[:20]}")
                 except UnicodeDecodeError as e:
+                    print(f"adjusting chunk at {offset + e.start} (offset: {offset})")
                     text = chunk[:e.start]
                     buffer = chunk[e.start:]
 
                 yield Chunk(offset, text)
 
 
+class IndexValidator:
+
+    def __init__(self, args):
+        self.args = args
+        self.reader = ByteReader(args.text)
+
+    def validate(self, articles: List[Article]):
+        for article in articles:
+            for element in article.elements:
+                snippet = self.reader.read(element.offset, len(element.bytes))
+                if snippet != element.bytes:
+                    caption = element.__class__.__name__
+
+                    raise ValueError(f"snippet does not match section at offset {element.offset}:\n"
+                                     f"{caption}: <<<{format_text(element.bytes)}>>>\n"
+                                     f"Snippet: <<<{format_text(snippet)}>>>")
+
+
+class IndexDumper:
+
+    def __init__(self, args):
+        self.args = args
+
+    def dump(self, articles: List[Article]):
+        print("Number of articles:", len(articles))
+        for article in articles:
+            print("." * 80)
+            for element in article.elements:
+                caption = element.__class__.__name__
+                print(f"{caption}: <<<{format_text(element.bytes)}>>>")
+            print("^" * 80)
+
+
 def main(args):
 
+    print("Building index...")
     builder = IndexBuilder(args)
     builder.build()
-    builder.dump()
+
+    print("Validating index...")
+    validator = IndexValidator(args)
+    validator.validate(builder.articles)
+
+    print("Dumping index...")
+    dumper = IndexDumper(args)
+    dumper.dump(builder.articles)
 
 
 if __name__ == '__main__':
