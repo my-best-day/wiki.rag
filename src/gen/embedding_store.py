16986e026503aa4f4517eef54232ae1dbcacca79
Ron Hirsch <ronhirsch@gmail.com>
2024-11-22 14:46:39 -0500

    big refactor + added unit test

--- src/gen/embedding_store.py
+++ src/gen/embedding_store.py
@@ -7,87 +7,6 @@ from typing import List, Tuple
 from numpy.typing import NDArray
 
 
-class EmbeddingStore:
-    """
-    Simple incremental store for embeddings.
-    """
-
-    def __init__(self, path):
-        self.path = Path(path)
-
-    @property
-    def lock_path(self):
-        return self.path.with_suffix(".lock")
-
-    def extend_embeddings(self, uids: List[UUID], embeddings: List[np.ndarray]) -> None:
-        """
-        Add embeddings to the store.
-        :param add_embeddings: List of tuples (segment_id, embedding)
-        """
-        str_uids = [str(uid) for uid in uids]
-        lock_path = self.lock_path
-        with CleanFileLock(lock_path):
-            self._add_embeddings(str_uids, embeddings)
-
-    def _add_embeddings(self, add_uids: List[str], add_embeddings: List[np.ndarray]):
-        """
-        Add embeddings to the store.
-        :param add_embedding_list: List of tuples (segment_id, embedding)
-        """
-        add_str_uids = [str(uid) for uid in add_uids]
-        np_str_uids, np_embeddings = self._load_embeddings()
-        str_uids, embeddings = np_str_uids.tolist(), np_embeddings.tolist()
-        str_uids.extend(add_str_uids)
-        embeddings.extend(add_embeddings)
-        np.savez(self.path, uids=str_uids, embeddings=add_embeddings)
-
-    def get_count(self) -> int:
-        """
-        Get the number of embeddings in the store.
-        :return: Number of embeddings in the store.
-        """
-        lock_path = self.lock_path
-        with CleanFileLock(lock_path):
-            if not self.path.exists():
-                count = 0
-
-            with np.load(self.path) as data:
-                count = len(data["indices"])
-        return count
-
-    def load_embeddings(self) -> Tuple[List[int], List[np.ndarray]]:
-        """
-        Load embeddings from the store.
-        Do type conversion and handle locks here.
-        :return: an array of uids: List[UUID] and an array of embeddings: List[np.ndarray]
-        """
-        lock_path = self.lock_path
-        with CleanFileLock(lock_path):
-            np_str_uids, np_embeddings = self._load_embeddings()
-            str_uids = np_str_uids.tolist()
-            uids = [UUID(uid) for uid in str_uids]
-            embeddings = np_embeddings.tolist()
-            return uids, embeddings
-
-    def _load_embeddings(self) -> Tuple[NDArray[str], NDArray[np.ndarray]]:  # type: ignore
-        """
-        Load embeddings from the store.
-        - It is the caller responsibility to very the file exists and handle
-          locks.
-        - this method returns a tuple of ndarrays.
-        :return: an ndarray of uids: NDArray[UUID] and an ndarray of embeddings: NDArray[np.ndarray]
-        """
-        if self.path.exists():
-            with np.load(self.path) as data:
-                np_str_uids = data["uids"]
-                np_embeddings = data["embeddings"]
-        else:
-            np_str_uids = np.array([], dtype=str)
-            np_embeddings = np.array([], dtype=np.ndarray)
-
-        return np_str_uids, np_embeddings
-
-
 class CleanFileLock(FileLock):
     """
     A file lock that cleans up the lock file after releasing the lock.
@@ -99,3 +18,94 @@ class CleanFileLock(FileLock):
         # Clean up the lock file after releasing the lock
         if os.path.exists(self.lock_file):
             os.unlink(self.lock_file)
+
+
+class EmbeddingStore:
+    """
+    Simple incremental store for embeddings.
+    """
+
+    # Indirection allows easier substitution for testing purposes
+    file_lock_class = CleanFileLock
+
+    def __init__(self, path):
+        self.path = Path(path)
+
+    @property
+    def lock_path(self):
+        return self.path.with_suffix(".lock")
+
+    def extend_embeddings(self, uids: List[UUID], embeddings: np.ndarray) -> None:
+        """
+        Add embeddings to the store.
+        Args:
+            uids: List of UUIDs
+            embeddings: List of embeddings (the output of a sentence_transformer.encode())
+        """
+        str_uids = [str(uid) for uid in uids]
+        lock_path = self.lock_path
+        with self.file_lock_class(lock_path):
+            self._add_embeddings(str_uids, embeddings)
+
+    def _add_embeddings(self, add_uids: List[str], add_embeds: np.ndarray):
+        """
+        Add embeddings to the store.
+        :param add_embedding_list: List of tuples (segment_id, embedding)
+        """
+        if len(add_uids) == 0:
+            return
+        np_str_uids, np_embeds = self._load_embeddings()
+        np_str_uids = np.concatenate((np_str_uids, add_uids)) if len(np_str_uids) else add_uids
+        np_embeds = np.concatenate((np_embeds, add_embeds)) if len(np_embeds) else add_embeds
+        np.savez(self.path, uids=np_str_uids, embeddings=np_embeds)
+
+    def get_count(self) -> int:
+        """
+        Get the number of embeddings in the store.
+        :return: Number of embeddings in the store.
+        """
+        lock_path = self.lock_path
+        with self.file_lock_class(lock_path):
+            np_str_uids, _ = self._load_embeddings()
+            count = len(np_str_uids)
+        return count
+
+    def load_embeddings(self) -> Tuple[List[int], List[np.ndarray]]:
+        """
+        Load embeddings from the store.
+        Do type conversion and handle locks here.
+        :return: an array of uids: List[UUID] and an array of embeddings: List[np.ndarray]
+        """
+        lock_path = self.lock_path
+        with self.file_lock_class(lock_path):
+            np_str_uids, np_embeddings = self._load_embeddings()
+            np_uids = np.vectorize(UUID)(np_str_uids)
+            return np_uids, np_embeddings
+
+    def _load_embeddings(self) -> Tuple[NDArray[str], np.ndarray]:  # type: ignore
+        """
+        Load embeddings from the store.
+        - It is the caller responsibility to very the file exists and handle
+          locks.
+        - this method returns a tuple of ndarrays.
+        :return: an ndarray of uids: NDArray[UUID] and an ndarray of embeddings: NDArray[np.ndarray]
+        """
+        if self.does_store_exist():
+            with np.load(self.path) as data:
+                np_str_uids = data["uids"]
+                np_embeddings = data["embeddings"]
+        else:
+            np_str_uids, np_embeddings = self.empty_result()
+
+        return np_str_uids, np_embeddings
+
+    # for testing purposes we move file.path.exists here
+    def does_store_exist(self) -> bool:
+        """Check if the store exists."""
+        return self.path.exists()
+
+    @staticmethod
+    def empty_result() -> Tuple[List[UUID], np.ndarray]:
+        uids = np.empty((0,), dtype=str)
+        embeddings = np.empty((0, 0), dtype=float)
+        return uids, embeddings
---
+++ tests/unit/test/gen/test_embedding_store.py
@@ -0,0 +1,256 @@
+import unittest
+import importlib
+import numpy as np
+import numpy.testing as npt
+from uuid import uuid4
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+from gen.embedding_store import CleanFileLock
+from gen.embedding_store import EmbeddingStore
+
+
+class CleanFileLockTestCase(unittest.TestCase):
+    def test_exit(self):
+        # test: lock file is removed after release
+        path = "/fake/path"
+        lock = CleanFileLock(path)
+        with patch("os.path.exists") as mock_exists:
+            mock_exists.return_value = True
+            with patch("os.unlink") as mock_unlink:
+                lock.__exit__(None, None, None)
+                mock_exists.assert_called_once_with(path)
+                mock_unlink.assert_called_once_with(path)
+
+
+class TestCleanFileLock:
+
+    instances = []
+
+    def __init__(self, *args, **kwargs):
+        print(f"TestCleanFileLock initialized with args: {args}, kwargs: {kwargs}")
+        self.args = args
+        self.kwargs = kwargs
+        self.enter_call_count = 0
+        self.exit_call_count = 0
+        TestCleanFileLock.instances.append(self)
+
+    def __enter__(self):
+        print("TestCleanFileLock acquired")
+        self.enter_call_count += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.exit_call_count += 1
+        print("TestCleanFileLock released")
+
+
+class TestEmbeddingStore(unittest.TestCase):
+
+    def setUp(self):
+        TestCleanFileLock.instances.clear()
+        EmbeddingStore.file_lock_class = TestCleanFileLock
+
+    def tearDown(self):
+        module = importlib.import_module(EmbeddingStore.__module__)
+        importlib.reload(module)
+
+    def test_lock_path(self):
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+        self.assertEqual(str(store.lock_path), f"{path}.lock")
+        self.assertIsInstance(store.lock_path, Path)
+
+    def test_extend_embeddings(self):
+        # test: lock is used, _add_embeddings was called, uids were cast to strings
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+        store._add_embeddings = MagicMock()
+
+        uids = [uuid4(), uuid4()]
+        stringified_uids = [str(uid) for uid in uids]
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+        store.extend_embeddings(uids, embeddings)
+
+        self.assertEqual(len(TestCleanFileLock.instances), 1)
+        file_lock = TestCleanFileLock.instances[0]
+        self.assertIsInstance(file_lock, TestCleanFileLock)
+        self.assertEqual(file_lock.enter_call_count, 1)
+        self.assertEqual(file_lock.exit_call_count, 1)
+        store._add_embeddings.assert_called_once_with(stringified_uids, embeddings)
+
+    def test_extend_embeddings_empty(self):
+        # test: lock is used, _add_embeddings was called, uids were cast to strings
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+        store._add_embeddings = MagicMock()
+
+        uids = []
+        stringified_uids = [str(uid) for uid in uids]
+        embeddings = []
+        store.extend_embeddings(uids, embeddings)
+
+        self.assertEqual(len(TestCleanFileLock.instances), 1)
+        file_lock = TestCleanFileLock.instances[0]
+        self.assertIsInstance(file_lock, TestCleanFileLock)
+        self.assertEqual(file_lock.enter_call_count, 1)
+        self.assertEqual(file_lock.exit_call_count, 1)
+        store._add_embeddings.assert_called_once_with(stringified_uids, embeddings)
+
+    def test_protected_add_embeddings_init(self):
+        # test: incremental addition... from scratch
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        empty_uids, empty_embeddings = EmbeddingStore.empty_result()
+        store._load_embeddings = MagicMock(return_value=(empty_uids, empty_embeddings))
+
+        np.savez = MagicMock()
+
+        uids = [uuid4(), uuid4()]
+        stringified_uids = [str(uid) for uid in uids]
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+        store._add_embeddings(stringified_uids, embeddings)
+
+        store._load_embeddings.assert_called_once()
+        np.savez.assert_called_once_with(Path(path), uids=stringified_uids, embeddings=embeddings)
+
+    def test_protected_add_embeddings_init_empty(self):
+        # test: incremental addition... from scratch
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        empty_uids, empty_embeddings = EmbeddingStore.empty_result()
+        store._load_embeddings = MagicMock(return_value=(empty_uids, empty_embeddings))
+
+        np.savez = MagicMock()
+
+        uids = []
+        stringified_uids = [str(uid) for uid in uids]
+        embeddings = np.array([])
+        store._add_embeddings(stringified_uids, embeddings)
+
+        store._load_embeddings.assert_not_called()
+        np.savez.assert_not_called()
+
+    def test_protected_add_embeddings_append(self):
+        # test: incremental addition with existing entries
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        existing_uids = np.array([str(uuid4()), str(uuid4())])
+        existing_embeddings = np.array([[7, 8, 9], [10, 11, 12]])
+        store._load_embeddings = MagicMock(return_value=(
+            existing_uids, existing_embeddings))
+
+        np.savez = MagicMock()
+
+        uids = [str(uuid4()), str(uuid4())]
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+        expected_uids = np.concatenate((existing_uids, uids))
+        expected_embeddings = np.concatenate((existing_embeddings, embeddings))
+        store._add_embeddings(uids, embeddings)
+
+        np.savez.assert_called_once()
+        _, kwargs = np.savez.call_args  # Get the call arguments
+        npt.assert_array_equal(kwargs["uids"], expected_uids)
+        npt.assert_array_equal(kwargs["embeddings"], expected_embeddings)
+
+    def test_protected_add_embeddings_append_empty(self):
+        # test: incremental addition with existing entries
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        existing_uids = np.array([str(uuid4()), str(uuid4())])
+        existing_embeddings = np.array([[7, 8, 9], [10, 11, 12]])
+        store._load_embeddings = MagicMock(return_value=(
+            existing_uids, existing_embeddings))
+
+        np.savez = MagicMock()
+
+        uids = []
+        embeddings = np.array([])
+        store._add_embeddings(uids, embeddings)
+
+        np.savez.assert_not_called()
+
+    def test_protected_add_embeddings_do_not_stringify(self):
+        # stringifying uids is the public method responsibility, don't do it here
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        empty_uids, empty_embeddings = EmbeddingStore.empty_result()
+        store._load_embeddings = MagicMock(return_value=(empty_uids, empty_embeddings))
+
+        np.savez = MagicMock()
+
+        uids = [uuid4(), uuid4()]
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+        store._add_embeddings(uids, embeddings)
+
+        store._load_embeddings.assert_called_once()
+        np.savez.assert_called_once_with(Path(path), uids=uids, embeddings=embeddings)
+
+    def test_get_count(self):
+        # test count for zero and none zero entries
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+
+        store._load_embeddings = MagicMock(return_value=(np.array([]), np.array([])))
+        self.assertEqual(store.get_count(), 0)
+
+        store._load_embeddings = MagicMock(return_value=(np.array(['s1', 's2']), np.array([])))
+        self.assertEqual(store.get_count(), 2)
+
+    def test_load_embeddings(self):
+        # test: lock is used, _load_embeddings was called, uids were cast to UUIDS
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+
+        uids = np.array([uuid4(), uuid4()])
+        stringified_uids = uids.astype(str)
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+
+        store._load_embeddings = MagicMock()
+        store._load_embeddings.return_value = (stringified_uids, embeddings)
+
+        out_uids, out_embeddings = store.load_embeddings()
+
+        self.assertEqual(len(TestCleanFileLock.instances), 1)
+        file_lock = TestCleanFileLock.instances[0]
+        self.assertIsInstance(file_lock, TestCleanFileLock)
+        self.assertEqual(file_lock.enter_call_count, 1)
+        self.assertEqual(file_lock.exit_call_count, 1)
+        npt.assert_array_equal(out_uids, uids)
+        npt.assert_array_equal(out_embeddings, embeddings)
+
+    def test_protected_load_embeddings_no_file(self):
+        # test: no store
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        store.does_store_exist = MagicMock(return_value=False)
+
+        uids, embeddings = store._load_embeddings()
+        self.assertEqual(uids.size, 0)
+        self.assertEqual(embeddings.size, 0)
+
+    def test_protected_load_embeddings(self):
+        # test: empty store
+        path = "/dev/null"
+        store = EmbeddingStore(path)
+        store.does_store_exist = MagicMock(return_value=True)
+
+        uids = [uuid4(), uuid4()]
+        stringified_uids = np.array([str(uid) for uid in uids])
+        embeddings = np.array([[1, 2, 3], [4, 5, 6]])
+        mocked_data = {"uids": stringified_uids, "embeddings": embeddings}
+
+        mock_np_load = MagicMock()
+        mock_np_load.return_value.__enter__.return_value = mocked_data
+
+        with patch("numpy.load", mock_np_load):
+            out_uids, out_embeddings = store._load_embeddings()
+            mock_np_load.assert_called_once_with(Path(path))
+            npt.assert_array_equal(out_uids, stringified_uids)
+            npt.assert_array_equal(out_embeddings, embeddings)
+
+    def test_does_store_exists(self):
+        # test: file exists
+        path = "/fake/path"
+        store = EmbeddingStore(path)
+        with patch.object(Path, "exists", return_value=False):
+            self.assertFalse(store.does_store_exist())
