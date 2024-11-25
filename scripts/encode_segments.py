94e042c4f8b864e0599df694d85b19f40bb4ceaf
Ron Hirsch <ronhirsch@gmail.com>
2024-11-25 09:39:59 -0500

    added incremental and max-items

--- scripts/encode_segments.py
+++ scripts/encode_segments.py
@@ -29,6 +29,8 @@ class SegmentEncoder:
         self.encoder = Encoder(batch_size=args.batch_size)
         embedding_store_path = Path(f"{args.path_prefix}_{args.max_len}_embeddings.npz")
         self.embedding_store = EmbeddingStore(embedding_store_path)
+        if not args.incremental and embedding_store_path.exists():
+            embedding_store_path.unlink()
 
     @property
     def segments(self):
@@ -40,8 +42,33 @@ class SegmentEncoder:
                              if isinstance(element, ExtendedSegment)]
         return extended_segments
 
+    @property
+    def segments_to_encode(self):
+        assert self.args.max_items >= 0, "max_items must be non-negative"
+
+        all_segments = self.segments
+
+        count = 0
+        if self.args.incremental:
+            count = self.embedding_store.get_count()
+            segments = all_segments[count:]
+        else:
+            segments = all_segments
+
+        if self.args.max_items > 0:
+            segments = segments[:self.args.max_items]
+
+        msg = f"{len(all_segments)} total segments, {count} processed, {len(segments)} pending"
+        logger.info(msg)
+
+        return segments
+
     def encode_segments(self):
-        segments = self.segments
+        segments = self.segments_to_encode
+        if not segments:
+            logger.info("No segments to encode")
+            return
+
         batch_size = self.args.batch_size
         buffer_length = self.args.buffer_length
         uids_buffer = []
@@ -59,7 +86,9 @@ class SegmentEncoder:
                 embedding_buffer.clear()
 
             # Process the batch here
-            logger.info(f"Processing batch {i // batch_size + 1}: {batch}")
+            current_batch = i // batch_size + 1
+            total_batches = (len(segments) + batch_size - 1) // batch_size
+            logger.info(f"Processing batch {current_batch} / {total_batches}")
 
         if uids_buffer:
             self.persist_embeddings(uids_buffer, embedding_buffer)
@@ -88,6 +117,10 @@ if __name__ == '__main__':
     parser.add_argument("-m", "--max-len", type=int, help="Maximum segment length")
     parser.add_argument("-bs", "--batch-size", type=int, help="Batch size")
     parser.add_argument("-bl", "--buffer-length", type=int, help="Buffer length")
+    parser.add_argument("-i", "--incremental", default=False, action="store_true",
+                        help="Incremental mode")
+    parser.add_argument("-mi", "--max-items", type=int, default=0,
+                        help="Maximum number of items to process (zero means no limit)")
     parser.add_argument("-d", "--debug", default=False, action="store_true", help="Debug mode")
     args = parser.parse_args()
 
@@ -112,6 +145,9 @@ if __name__ == '__main__':
 
     if args.buffer_length is None:
         parser.error("Please provide the buffer length")
+
+    if args.max_items < 0:
+        parser.error("max_items must be non-negative (zero means no limit)")
 
     main(args)
     logger.info(f"Elapsed time: {time.time() - t0:.2f} seconds")
