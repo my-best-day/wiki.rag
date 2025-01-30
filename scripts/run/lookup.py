486733b1811ef007e39c4cc0cdfd6fe89d215c72
Ron Hirsch <ronhirsch@gmail.com>
2025-01-30 09:03:47 -0500

    fixed for segments using CombinedService

--- scripts/run/lookup.py
+++ scripts/run/lookup.py
@@ -4,29 +4,40 @@ import sys
 import time
 import logging
 import argparse
-from uuid import UUID
-from pathlib import Path
-from typing import List, Tuple, Union
+import os
 
+from search.stores import Stores
+from xutils.app_config import AppConfig, load_app_config
 from gen.element.element import Element
-from gen.element.extended_segment import ExtendedSegment
-from gen.element.flat.flat_extended_segment import FlatExtendedSegment
-from search.stores.stores_flat import StoresFlat
-from search.stores.stores import Stores
 from search.k_nearest_finder import KNearestFinder
-from xutils.embedding_config import EmbeddingConfig
+from search.services.combined_service import CombinedService, CombinedRequest, Kind
+from search.services.combined_service import CombinedResponse
 
 
 class LookupCLI:
-    def __init__(self, args, embedding_config):
-        self.args = args
-        self.embedding_config = embedding_config
+    def __init__(
+        self,
+        logger: logging.Logger,
+        app_config: AppConfig,
+        k_nearest: int,
+        threshold: float,
+        max_documents: int,
+    ) -> None:
+        self.app_config = app_config
+        self.k_nearest = k_nearest
+        self.threshold = threshold
+        self.max_documents = max_documents
 
-        stores_class = Stores if args.skip_flat else StoresFlat
-        self.stores = stores_class(args.text, embedding_config)
+        text_file_path = app_config.text_file_path
+        embedding_config = app_config.embed_config
 
-        self.k_nearest_finder = KNearestFinder(self.stores, embedding_config)
-        self.stores.background_load()
+        stores = Stores.create_stores(text_file_path, embedding_config)
+        stores.background_load()
+
+        finder = KNearestFinder(stores, embedding_config)
+
+        self.service = CombinedService(stores, embedding_config, finder)
+
         self._extended_segment_map = None
         self._article_map = None
 
@@ -36,19 +47,39 @@ class LookupCLI:
 
     def run_segments(self):
         while True:
-            prompt = "Enter a query sentence (or 'exit' to quit): "
-            print(prompt, " Press Cmd-D when done: ")
-            query = sys.stdin.read().strip()
-            print(".... ... .. .")
+            query = self.get_query()
             if query.lower() in ("exit", "quit"):
                 break
 
             t0 = time.time()
-            nearest_segments: List[Tuple[UUID, float]] = \
-                self.k_nearest_finder.find_k_nearest_segments(query, self.args.k_nearest)
+            response = self.get_response(query)
             elapsed = time.time() - t0
 
-            self.display_nearest_segments(query, nearest_segments, elapsed)
+            results = response.results
+            self.display_nearest_segments(query, results, elapsed)
+
+    def get_query(self) -> str:
+        prompt = "Enter a query sentence (or 'exit' to quit): "
+        print(prompt, " Press Cmd-D when done: ")
+        query = sys.stdin.read().strip()
+        print(".... ... .. .")
+        return query
+
+    def get_response(self, query) -> CombinedResponse:
+        k_nearest = self.k_nearest
+        threshold = self.threshold
+        max_documents = self.max_documents
+
+        combined_request = CombinedRequest(
+            action="search",
+            kind=Kind.Segment,
+            query=query,
+            k=k_nearest,
+            threshold=threshold,
+            max=max_documents
+        )
+        combined_response = self.service.combined(combined_request)
+        return combined_response
 
     def run_articles(self):
         while True:
@@ -60,50 +91,36 @@ class LookupCLI:
                 break
 
             t0 = time.time()
-            top_k_article_similarities: List[Tuple[UUID, float]] = \
-                self.k_nearest_finder.find_k_nearest_articles(query, self.args.k_nearest, 0.75, 5)
+            # : List[Tuple[UUID, float]] = \
+            top_k_article_similarities = \
+                self.k_nearest_finder.find_k_nearest_articles(query, self.k_nearest, 0.75, 5)
             elapsed = time.time() - t0
 
             self.display_nearest_articles(query, top_k_article_similarities, elapsed)
 
     def display_nearest_segments(self, query, nearest_segments, elapsed):
-
-        print("\n\n\n\n")
+        print('\n' * 5)
         print("Nearest Segments ", ">>>> " * 10)
         print("Nearest Segments ", ">>>> " * 10)
         print("QUERY:", query)
         print(f"query length: {len(query)}, {elapsed:.4f}s" + "---- " * 10)
-        for i, (uid, score) in enumerate(nearest_segments):
-            segment = self.get_extended_segment(uid)
 
-            text = self.format_ext_segment(segment)
+        for i, result_element in enumerate(nearest_segments):
+            score = result_element.similarity
 
-            print(f"{i + 1}. Segment ID: {uid}, Score: {score:.4f}, "
-                  f"(off: {segment.offset}, len: {len(segment.text)})")
-            print(f"Article: {segment.article.header.text.strip()}")
+            segment_record = result_element.record
+            index = segment_record.segment_index
+            offset = segment_record.offset
+            length = segment_record.length
+
+            caption = result_element.caption
+            text = result_element.text
+
+            print(f"{i + 1}. Segment Index: {index}, Score: {score:.4f}, "
+                  f"(off: {offset}, len: {length})")
+            print(f"Article: {caption}")
             print(text)
             print("<--- " * 10, "\n")
-
-    def format_ext_segment(self, ext_segment: Union[ExtendedSegment, FlatExtendedSegment]) -> str:
-        if isinstance(ext_segment, FlatExtendedSegment):
-            text = self.format_flat_extended_segment(ext_segment)
-        elif isinstance(ext_segment, ExtendedSegment):
-            text = self.format_compound_extended_segment(ext_segment)
-        else:
-            raise TypeError
-
-        return text
-
-    @staticmethod
-    def format_compound_extended_segment(extended_segment: ExtendedSegment) -> str:
-        text = extended_segment.before_overlap.text if extended_segment.before_overlap else ""
-        text += "][" + extended_segment.segment.text + "]["
-        text += extended_segment.after_overlap.text if extended_segment.after_overlap else ""
-        return text
-
-    @staticmethod
-    def format_flat_extended_segment(flat_extended_segment):
-        return flat_extended_segment.text
 
     def display_nearest_articles(self, query, top_k_article_similarities, elapsed):
         print("\n\n\n\n")
@@ -132,57 +149,44 @@ class LookupCLI:
         return Element.instances[uid]
 
 
-def main(args, embedding_config):
-    lookup_cli = LookupCLI(args, embedding_config)
+def get_app_config(logger) -> AppConfig:
+    config_file = "config.ini"
+    if "CONFIG_FILE" in os.environ:
+        config_file = os.environ["CONFIG_FILE"]
+    logger.debug(f"Using config file: {config_file}")
+
+    app_config = load_app_config(config_file)
+    logger.info(f"AppConfig: {app_config}")
+
+    return app_config
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Show random paragraphs from a JSON file.")
+    parser.add_argument("-k", "--k-nearest", type=int, default=5,
+                        help="Number of nearest neighbors")
+    parser.add_argument("--threshold", type=float, default=0.65)
+    parser.add_argument("--max-documents", type=int, default=3)
+    parser.add_argument("--log-level", type=str, default="INFO")
+    args, _ = parser.parse_known_args()
+
+    log_level_upper = args.log_level.upper()
+    numeric_level = getattr(logging, log_level_upper, logging.INFO)
+    logging.basicConfig(level=numeric_level)
+    logging.getLogger("datasets").setLevel(logging.WARNING)
+    logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
+
+    logger = logging.getLogger(__name__)
+
+    app_config = get_app_config(logger)
+
+    k_nearest = args.k_nearest
+    threshold = args.threshold
+    max_documents = args.max_documents
+
+    lookup_cli = LookupCLI(logger, app_config, k_nearest, threshold, max_documents)
     lookup_cli.run()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.getLogger("datasets").setLevel(logging.WARNING)
-    logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
-
-    parser = argparse.ArgumentParser(description="Show random paragraphs from a JSON file.")
-    parser.add_argument("-t", "--text", type=str, help="Path to the text file")
-    parser.add_argument("-pp", "--path-prefix", type=str, help="Prefix of the element files")
-    parser.add_argument("-m", "--max-len", type=int, help="Maximum segment length")
-    parser.add_argument("-d", "--dim", type=int, help="dimension, optional")
-    parser.add_argument('-s', '--stype', type=str, help='Stype'
-                        , choices=["float32", "float16", "int8", "uint8"])
-    parser.add_argument('--norm-type', type=str, choices=["float32", "float16", "int8", "uint8"],
-                        help='Data type to use for L2 normalization (default: input type)')
-    parser.add_argument('--l2-normalize', action='store_true', default=False,
-                        help='l2 normalize the embeddings')
-    parser.add_argument("--skip-flat", default=False, action="store_true",
-                        help="Don't use flat segments")
-    parser.add_argument('-f', '--force', action='store_true', help='Force overwrite')
-    parser.add_argument("--debug", default=False, action="store_true", help="Debug mode")
-    parser.add_argument("-k", "--k-nearest", type=int, default=5,
-                        help="Number of nearest neighbors")
-    args = parser.parse_args()
-
-    if args.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-
-    if args.text is None:
-        parser.error("Please provide the path to the text file")
-    args.text = Path(args.text)
-    if not args.text.exists():
-        parser.error(f"File {args.text} not found")
-
-    if args.path_prefix is None:
-        parser.error("Please provide the path prefix")
-
-    if args.max_len is None:
-        parser.error("Please provide the maximum segment length")
-
-    embedding_config = EmbeddingConfig(
-        prefix=args.path_prefix,
-        max_len=args.max_len,
-        dim=args.dim,
-        stype=args.stype,
-        norm_type=args.norm_type,
-        l2_normalize=args.l2_normalize
-    )
-
-    main(args, embedding_config)
+    main()
